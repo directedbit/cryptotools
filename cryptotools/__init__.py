ed4292bdb3fba5db37ab4706b4b9ed44dbecb952
Richard Druce <1556051+directedbit@users.noreply.github.com>
2025-07-16 20:28:54 +1000

    Updates to make it compatible with 3.12

--- cryptotools/__init__.py
+++ cryptotools/__init__.py
@@ -3,6 +3,7 @@ Zero dependency implementation of common cryptographic functions for working wit
 """
 
 import pkgutil
+import importlib
 
 from .BTC import *
 from .BTC.HD import *
@@ -10,10 +11,11 @@ from .ECDSA import *
 from .transformations import *
 from .number_theory_stuff import *
 
-__all__ = []
+__all__: list[str] = []
 
-for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
-    _module = loader.find_module(module_name).load_module(module_name)
+for module_info in pkgutil.walk_packages(__path__):
+    module_name = module_info.name
+    _module = importlib.import_module(f"{__name__}.{module_name}")
     module_exports = getattr(_module, '__all__', [])
     __all__.extend(module_exports)
 
